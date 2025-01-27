45f658ac3f3dcea81d55370aeea77acb7ebf79ee
Leonid <leoong55@gmail.com>
2025-01-27 03:48:18 -0500

    Add files

---
+++ plugins/mitogen-0.2.10/.ci/ansible_install.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import ci_lib
+
+batches = [
+    [
+        # Must be installed separately, as PyNACL indirect requirement causes
+        # newer version to be installed if done in a single pip run.
+        # Separately install ansible based on version passed in from azure-pipelines.yml or .travis.yml
+        'pip install "pycparser<2.19" "idna<2.7"',
+        'pip install '
+            '-r tests/requirements.txt '
+            '-r tests/ansible/requirements.txt',
+        'pip install -q ansible=={0}'.format(ci_lib.ANSIBLE_VERSION)
+    ],
+    [
+        'aws ecr-public get-login-password | docker login --username AWS --password-stdin public.ecr.aws',
+    ]
+]
+
+batches[-1].extend([
+    'docker pull %s' % (ci_lib.image_for_distro(distro),)
+    for distro in ci_lib.DISTROS
+])
+
+ci_lib.run_batches(batches)
